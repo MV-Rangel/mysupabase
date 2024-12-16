08dbc2bb7a5e314f7871dc178782595f87271394
Marcus <mrangelpedagogo@gmail.com>
2024-12-16 09:51:56 -0300

    Changes

---
+++ src/supabase.js
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import duration from "dayjs/plugin/duration";
+dayjs.extend(duration);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const supabaseUrl = "https://cqidqrqcwnptczzmhdwi.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxaWRxcnFjd25wdGN6em1oZHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NTA4MjksImV4cCI6MjA0OTUyNjgyOX0.ahANrBL_lrvMdXWVLMhP_h-1GvUzOwzZay7x3zP2wFI";
+const supabase = createClient(supabaseUrl, supabaseKey);
